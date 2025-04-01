d428eda14835120d6c0bdbbea292005dc030a7aa
ShufflesCards <158957446+ShufflesCards@users.noreply.github.com>
2025-04-01 13:54:08 -0400

    Update howToClassPygame.py

--- howToClassPygame.py
+++ howToClassPygame.py
@@ -5,8 +5,9 @@ import pygame
 
 class Box(pygame.sprite.Sprite):
     time = 0
+
     # describe self as the object, the instance of the class
-    def __init__ (self, x, y, img, dx=0, dy=0): #time in seconds
+    def __init__ (self, x, y, img, dx=0, dy=0, imageScalel=1): #time in seconds
         super().__init__() # Box is a child class calling its parent pygame.sprite.Sprite
 
         self.x = x
@@ -14,29 +15,52 @@ class Box(pygame.sprite.Sprite):
         self.dx = dx
         self.dy = dy
         # x pos, y pos, x velocity, y velocity
-        self.image = pygame.transform.scale_by(pygame.image.load(img), 0.05)#.convert_alpha()
+        self.imageScale = 0.05
+        self.imageWidth = (pygame.image.load(img).get_width())*self.imageScale
+        self.imageHeight = (pygame.image.load(img).get_height())*self.imageScale
+        self.image = pygame.transform.scale_by(pygame.image.load(img), self.imageScale)#.convert_alpha()
         self.rect = self.image.get_rect(center = (200, 200))
 
+        self.visualHitbox = pygame.Surface([self.imageWidth, self.imageHeight])
+
+
     def __str__(self):
-        return f"({self.x}, {self.y})   <{self.dx}, {self.dy}> at {self.time} seconds"
-    
-    def increaseTime(self, dt):
+        return f"({self.x}, {self.y})   <{self.dx}, {self.dy}> at {self.time} units of time"
+    def increaseSpeedX(self, dx): # do not use negative numbers
+        if self.dx>0:
+            self.dx+=dx
+        else:
+            self.dx-=dx
+    def increaseSpeedY(self, dy): # do not use negative numbers
+        if self.dx<0:
+            self.dy-=dy
+        else:
+            self.dy+=dy
+
+    def increaseTime(self, dt=1):
         self.time+=dt
         self.update(dt)
 
     def update(self, dt=1):
         self.x+=dt*self.dx
         self.y+=dt*self.dy
+
     def draw(self, screen):
         screen.blit(self.image, (self.x,self.y)) # blit using the rect
+        screen.blit(self.visualHitbox, (self.x, self.y))
 
+
+    
+
+
+        
 clock = pygame.time.Clock()
 background_colour = (234, 212, 252) 
 screen = pygame.display.set_mode((1200, 800)) 
 pygame.display.set_caption('Window Caption') 
 screen.fill(background_colour) 
 pygame.display.flip() 
-thing = Box(50, 50, 'theBox.png', 2, 4)
+thing = Box(50, 50, 'theBox.png', 2, 2, 0.05)
 thing.increaseTime(4)
 print(thing)
 
@@ -52,10 +76,19 @@ while True:
             exit()
     screen.fill(background_colour) 
     thing.draw(screen)
-    thing.update()
-    if clock.get_time()%10==0:
+    thing.increaseTime()
+    if thing.x >1200 or thing.x <0:
+        if thing.dx>5 or thing.dx<-5:
+            thing.dx = -thing.dx//1.2
+        else:
+            thing.dx =-thing.dx
+    if thing.y >800 or thing.y < 0:
+        if thing.dy>5 or thing.dy<-5:
+            thing.dy = -thing.dy//1.2
+        else:
+            thing.dy= -thing.dy
 
-        print(thing)
+    
     pygame.display.flip()
     clock.tick(60)
 
